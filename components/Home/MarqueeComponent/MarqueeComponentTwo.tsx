206caf59bbf270070ad42bbcd6679000e139be8e
Korede <osokokoredepeter@gmail.com>
2025-03-17 18:56:53 +0100

    added key id on second Marquee Component

--- components/Home/MarqueeComponent/MarqueeComponentTwo.tsx
+++ components/Home/MarqueeComponent/MarqueeComponentTwo.tsx
@@ -10,7 +10,7 @@ const MarqueeComponentTwo = () => {
     <Marquee speed={100} delay={0} className='bg-lime-300 py-6 rotate-3'>
         {marqueeitems.map((item) => {
                 return(
-                    <p className='font-[Freckle_Face] text-7xl px-20 uppercase text-slate-950'>{item.label}</p>
+                    <p key={item.id} className='font-[Freckle_Face] text-7xl px-20 uppercase text-slate-950'>{item.label}</p>
                 );
             })}
     </Marquee>
